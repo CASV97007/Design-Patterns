38f77f01beb27d0003de551aae296acf65de471f
Christan Ariel Salazar Verduga <ariel-2h@hotmail.com>
2025-03-08 19:09:55 +0100

    Star with a simple SimUDuck app.

---
+++ INTO-DESSIGN_PATTERNS/com/casv/simuduckapp/SimUDuck.java
@@ -0,0 +1,17 @@
+package com.casv.simuduckapp;
+
+/**
+ * The game can show a large variety of
+ * duck species swimming and making quacking sounds. The initial
+ * designers of the system used standard OO techniques and created
+ * one Duck superclass from which all other duck types inherit.
+ * 
+ * @author Christan
+ */
+public class SimUDuck {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
---
+++ INTO-DESSIGN_PATTERNS/com/casv/simuduckapp/model/Duck.java
@@ -0,0 +1,33 @@
+package com.casv.simuduckapp.model;
+
+import java.util.logging.Logger;
+
+/**
+ * All ducks quack and swim, the
+ * superclass takes care of the
+ * implementation code.
+ * 
+ * @author
+ */
+public abstract class Duck {
+    Logger log = Logger.getLogger(getClass().getName());
+
+    public void quack() {
+        log.warning("quacking");
+    }
+
+    public void swim() {
+        log.warning("swimming");
+
+    }
+
+    /**
+     * Each duck subtype
+     * is responsible for
+     * implementing its own
+     * display() behavior
+     * for how it
+     */
+    public abstract void display();
+    // OTHER duck-like methods...
+}
